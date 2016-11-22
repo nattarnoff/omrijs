a02676bc0c996fc2414fb8f58f9a026217c28ec4
Nat Tarnoff <nattarnoff@gmail.com>
2016-11-22 11:15:30 -0600

    Moved demo switch to JS.

--- js/omri.js
+++ js/omri.js
@@ -15,47 +15,41 @@ function omri(options){
     }*/
   };
 
-
-
   var  _d = document,
     _w = window,
     $b = _d.querySelector('body')
-    ,$1st = $b.firstChild
+    , $1st = $b.firstChild
     , node = _d.createElement('div')
     , label = _d.createElement('label')
     , tog = _d.createElement('span')
-    , status = _d.createElement('span')
+    , statusOn = _d.createElement('span')
+    , statusOff = _d.createElement('span')
+    , switchWrapper = _d.createElement('span')
     , input = _d.createElement('input');
 
     if(!$b.classList.contains(options.animationClass)){
       $b.classList.add(options.animationClass);
     };
-    //if(document.body != null){ document.body.appendChild(template); }
-    $b.insertBefore(node, $1st);
-    node.appendChild(label).setAttribute('for','toggle');
-    node.appendChild(input).setAttribute('type','checkbox');
-    input.setAttribute('id','toggle');
-    input.classList.add('visuallyHidden');
-    label.textContent= options.animationLabel;
-    label.appendChild(status);
-    node.classList.add('animation-switch');
-    status.classList.add('status');
-    status.textContent = " "+ options.statusLabel;
-    label.appendChild(tog).classList.add('switch-wrapper');
 
+    $b.insertBefore(node, $1st);
+    node.classList.add('animation-switch');
+    node.appendChild(label).setAttribute('for','animationToggle');
+    label.textContent= options.animationLabel;
+    label.appendChild(switchWrapper).classList.add('switch-wrapper');
+    switchWrapper.appendChild(statusOn).classList.add('on','status');
+    statusOn.textContent = "On";
+    switchWrapper.appendChild(statusOff).classList.add('off','status')
+    statusOff.textContent = 'Off';
+    label.insertBefore(input, label.firstChild).setAttribute('type','checkbox');
+    input.setAttribute('id','animationToggle');
+    input.classList.add('visuallyHidden');
+    input.classList.add('toggle');
+    if(options.defaultState === true){
+      input.setAttribute('checked', 'checked');
+    };
     // function isChecked(){
     //look to see if the toggle is checked
     //if toggle:checked, remove active class from on
     //add active class to off
   //}
 };
-
-
-
-
-/*<div class="animation-switch">
-  <label for="checkbox">Animation is <span class="status">on</span>
-  <span class='switch-wrapper'>
-  </span></label>
-  <input type="checkbox" id="checkbox" class="visuallyHidden"/>
-</div>*/
